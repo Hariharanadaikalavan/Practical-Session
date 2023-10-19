503b5f93d760be88411e46ed4f505f90b895d3fb
hariharanadaikalavan <hari.ramu16@gmail.com>
2023-10-19 23:50:02 +0530

    Switchcase

---
+++ JavaSciprit/Switch/switch.js
@@ -0,0 +1,17 @@
+var a=100;
+var b=500;
+ var ch=2;
+
+ switch (ch)
+ {
+    case 1:
+        var add=a+b;
+        console.log(add);
+        break;
+    case 2:
+        var mul=a*b;
+        console.log(mul);
+        break;
+    default:
+        console.log("check input");    
+ }
