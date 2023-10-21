8186edb134a0fc38eb605c6fecba2c997715450c
hariharanadaikalavan <hari.ramu16@gmail.com>
2023-10-21 23:51:09 +0530

    Assignedment oprator

---
+++ JavaSciprit/formstyle/conditional.js
@@ -0,0 +1,32 @@
+//Bit wise oprators
+
+// var a=10;
+// var b=20;
+// console.log(a&b);
+
+//arrays
+// var a=[2,0.2,"chitra",'A',true,'A'];
+// //console.log(a);
+// console.log(typeof(a));
+// console.log(a[3]);
+// console.log(a.push(1000));
+// console.log(a);
+// console.log(a.unshift("one"));
+// console.log(a);
+// console.log(a.splice(2,0,"JavaScript"));
+// console.log(a);
+// console.log(a.pop());
+// console.log(a);
+// console.log(a.shift());
+// console.log(a);
+// console.log(a.splice(2,3,"js"));
+// console.log(a);
+// console.log(a.length);
+// console.log(a);
+
+
+// var name=prompt("Enter you name repectively");
+// var result=alert("Greeting from KGISL hi!" +" "+ name +" " +"enjoy our service");
+
+var a=['Orange',25,100,true,33.58];
+console.log(a);
