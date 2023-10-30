df912edbd19c4bc6ed4f90c9b044654a0fcf8d81
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-10-30 22:42:16 +0530

    getmonths

---
+++ JavaSciprit/Date/date.js
@@ -0,0 +1,20 @@
+
+//document.getElementById("div1").innerHTML=new Date();
+
+//console.log(d1);
+
+// function btn(){
+//     var d1=document.getElementById("input1").value;
+//     var d1=new date(d1);
+//     var d2=d1.getDay()+95;
+//     var d3=d1.setDate(d2);
+//     document.getElementById("input2").innerHTML=d3;
+    
+// }
+function btn(){
+document.getElementById('btnn').style.backgroundColor="red";
+
+
+//alert(input1)
+}
+document.getElementById('btnn').style.backgroundColor="";
