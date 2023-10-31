64f918be74f9caef77d20524a3ea59ef92a9419b
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-10-31 21:33:57 +0530

    strings

---
+++ JavaSciprit/Strings/strings.js
@@ -0,0 +1,24 @@
+// var name="hari haran";
+// console.log(name);
+// console.log(name.split (" "));
+// console.log(name.replace("haran","hari"));
+// // var a=name.startsWith("h")
+// // console.log(a);
+// // console.log(name.chatAt(5));
+
+
+// let num1=2;
+// console.log(Math.pow(num1,3));
+// var num2=144;
+// console.log(Math.sqrt(num2,2));
+
+var a=[1,2,3,4,5];
+// if(a.indexOf(1)>a.indexOf(2))
+// {
+//     console.log("great");
+// }
+// else
+// {
+//     console.log(low);
+// }
+console.log(Math.min(a));
