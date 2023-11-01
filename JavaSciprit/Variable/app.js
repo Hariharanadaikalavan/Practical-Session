7182aadcc7252268383941eee9e01e203cdb764f
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-01 21:54:17 +0530

    variable

---
+++ JavaSciprit/Variable/app.js
@@ -0,0 +1,15 @@
+//Task-1
+
+// var a="Coimbatore";
+// var b="Full_Stack_Developer";
+// console.log("You will be" ,b,"in",a);
+
+//Task-2
+
+// var firstName="Ram";lastName="Kumar";age=30;
+// console.log(firstName+lastName+age);
+
+//Task-3
+var birthYear=2000;
+var futureYear=2001;
+
