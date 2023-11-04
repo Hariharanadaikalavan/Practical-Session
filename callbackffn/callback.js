d71664bff7dfac9220a2bab1262ec6f9f52f991c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-04 22:52:45 +0530

    call back function

---
+++ callbackffn/callback.js
@@ -0,0 +1,11 @@
+function mainfun(getsubFun){
+    console.log("main function executed");
+    console.log(getsubFun)
+}
+
+mainfun()
+
+function subFun(){
+    console.log("subfunction is executed");
+}
+
